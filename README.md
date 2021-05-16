16317b332d95c6d247ad3c7dbd34a56fa81ddc6e
Ricky <nguyenanhgiau1008@gmail.com>
2021-05-16 12:17:30 +0700

    support package image for downloading

--- README.md
+++ README.md
@@ -2,3 +2,25 @@
 
 For the main project, see [https://github.com/csimmonds/a4rpi-local-manifest](https://github.com/csimmonds/a4rpi-local-manifest)
 
+# Flash image to sdcard
+If the flashing machine and the building machine is the same, just run command:
+```bash
+$ cd $ANDROID_BUILD_TOP
+$ ./scripts/android_flash_rpi4.sh sdb #suppose your sdcard is sdb
+```
+In the case, your flashing machine is difference from the building machine.<br>
+You have to package image and download it to your flashing machine.<br>
+Then, you can flash image to sdcard.
+## Package image for downloading
+After building, you can package your image by command:
+```bash
+$ ./scripts/package_image.sh
+```
+After packing, you will get a folder that contains all files output, includes script flash image.<br>
+Now, you can download this folder to your flashing machine and flash it to sdcard.<br>
+```bash
+$ cd android_image #cd to folder that contains your image
+$ ./android_flash_rpi4.sh sdb #suppose your sdcard is sdb
+```
+
+
---
+++ package_image.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+# Check image output
+if [ -z ${ANDROID_PRODUCT_OUT} ]; then
+	echo "You must run lunch first"
+	exit 1
+fi
+
+if [ -z ${TARGET_PRODUCT} ]; then
+	echo "Please run 'lunch' first"
+	exit
+fi
+
+if [ -z ${ANDROID_BUILD_TOP}/kernel/arpi/arch/arm64/boot/Image.gz ]; then
+	echo "Kernel is not exist"
+fi
+
+if [ -z ${ANDROID_BUILD_TOP}/kernel/arpi/arch/arm64/boot/dts/broadcom/bcm2711-rpi-4-b.dtb ] | \
+	[ -z ${ANDROID_BUILD_TOP}/kernel/arpi/arch/arm64/boot/dts/overlays/vc4-kms-v3d-pi4.dtbo ]; then
+	echo "Device tree is not exist"
+fi
+
+# Create folder contains your image
+PACKAGE_IMG_DIR=${ANDROID_BUILD_TOP}/android_image
+mkdir ${PACKAGE_IMG_DIR}
+
+cd ${ANDROID_BUILD_TOP}
+echo "Copying android image..."
+cp --parents out/target/product/rpi4/system.img ${PACKAGE_IMG_DIR}
+cp --parents out/target/product/rpi4/vendor.img ${PACKAGE_IMG_DIR}
+cp --parents out/target/product/rpi4/ramdisk.img ${PACKAGE_IMG_DIR}
+echo "Copying kernel image..."
+cp --parents -r device/arpi/rpi4/boot/ ${PACKAGE_IMG_DIR}
+cp --parents kernel/arpi/arch/arm64/boot/Image.gz ${PACKAGE_IMG_DIR}
+cp --parents kernel/arpi/arch/arm64/boot/dts/broadcom/bcm2711-rpi-4-b.dtb ${PACKAGE_IMG_DIR}
+mkdir ${PACKAGE_IMG_DIR}/overlays
+cp --parents kernel/arpi/arch/arm64/boot/dts/overlays/vc4-kms-v3d-pi4.dtbo ${PACKAGE_IMG_DIR}
+
+echo "Copying script flash for rpi4"
+sed '2 a ANDROID_BUILD_TOP=.\nANDROID_PRODUCT_OUT=./out/target/product/rpi4\nTARGET_PRODUCT=rpi4' ${ANDROID_BUILD_TOP}/scripts/android_flash_rpi4.sh \
+	> ${PACKAGE_IMG_DIR}/android_flash_rpi4.sh
+chmod a+x ${PACKAGE_IMG_DIR}/android_flash_rpi4.sh
+
+echo "Your image is located at ${ANDROID_BUILD_TOP}/android_image"
+echo "Now, you can download it and flash image to sdcard at local machine"
+echo "Package done!!!"
