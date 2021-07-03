167b27a04dbf68e8b79129d80a5106d1fcc4e68c
GiauNguyen <giau.nguyen2@datalogic.com>
2021-07-03 12:37:03 +0700

    support prebuilt kernel image v5.10

--- android_flash_rpi4.sh
+++ android_flash_rpi4.sh
@@ -105,17 +105,17 @@ if [ $? != 0 ]; then echo "ERROR"; exit; fi
 sudo cp ${ANDROID_BUILD_TOP}/device/arpi/rpi4/boot/* /mnt
 if [ $? != 0 ]; then echo "ERROR"; exit; fi
 
-sudo cp ${ANDROID_BUILD_TOP}/kernel/arpi/arch/arm64/boot/Image.gz /mnt
+sudo cp ${ANDROID_BUILD_TOP}/scripts/kernel-android-S/arpi/arch/arm64/boot/Image.gz /mnt
 if [ $? != 0 ]; then echo "ERROR"; exit; fi
 
-sudo cp ${ANDROID_BUILD_TOP}/kernel/arpi/arch/arm64/boot/dts/broadcom/bcm2711-rpi-4-b.dtb /mnt
+sudo cp ${ANDROID_BUILD_TOP}/scripts/kernel-android-S/arpi/arch/arm64/boot/dts/broadcom/bcm2711-rpi-4-b.dtb /mnt
 if [ $? != 0 ]; then echo "ERROR"; exit; fi
 
 sudo mkdir /mnt/overlays
-sudo cp ${ANDROID_BUILD_TOP}/kernel/arpi/arch/arm64/boot/dts/overlays/vc4-kms-v3d-pi4.dtbo /mnt/overlays
+sudo cp ${ANDROID_BUILD_TOP}/scripts/kernel-android-S/arpi/arch/arm64/boot/dts/overlays/vc4-kms-v3d-pi4.dtbo /mnt/overlays
 if [ $? != 0 ]; then echo "ERROR"; exit; fi
 
-sudo cp ${ANDROID_BUILD_TOP}/out/target/product/rpi4/ramdisk.img /mnt
+sudo cp ${ANDROID_PRODUCT_OUT}/ramdisk.img /mnt
 if [ $? != 0 ]; then echo "ERROR"; exit; fi
 
 sync
